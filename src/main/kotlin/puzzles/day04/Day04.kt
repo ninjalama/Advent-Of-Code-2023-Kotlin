d000cd65e78b23e7545f20d09d031ed7d86a5501
Øyvind Høisæther <ninjalama@gmail.com>
2023-12-10 13:33:46 +0100

    Part 1 - Sample

---
+++ src/main/kotlin/puzzles/day04/Day04.kt
@@ -0,0 +1,52 @@
+package puzzles.day03.two
+
+import utils.ResourceUtils
+
+data class ScratchCard(val line: String) {
+    val cardId: Int
+
+    private val winningNumbers: IntArray
+    private val cardNumbers: IntArray
+
+    init {
+        val (cardIdStr, cardSetStr) = line.split(":")
+        val (winningNumbersStr, cardNumbersStr) = cardSetStr.split("|")
+
+        cardId = cardIdStr.substring("Card ".length).toInt()
+        winningNumbers = winningNumbersStr.trim().replace("  ", " ").split(" ").map { it.toInt() }.toIntArray()
+        cardNumbers = cardNumbersStr.trim().replace("  "," ").split(" ").map { it.toInt() }.toIntArray()
+    }
+
+    fun getWinningNumbers(): Set<Int> {
+        return cardNumbers.toSet().intersect(winningNumbers.toSet())
+    }
+
+    companion object {
+        fun getPoints(nrOfWinningNumbers: Int): Int {
+            return when {
+                nrOfWinningNumbers <= 0 -> 0
+                else -> (0 until nrOfWinningNumbers).fold(0) { accumulator, number ->
+                    if (number == 0) accumulator + 1 else accumulator * 2
+                }
+            }
+        }
+    }
+}
+
+
+
+fun main(args: Array<String>) {
+
+    fun part1() {
+        println("PART 1 - SAMPLE")
+        val sampleStringList: List<String> = ResourceUtils.getResourceAsText("/day04/sampleInput.txt").orEmpty().split("\n")
+        val scratchCards = sampleStringList.map(::ScratchCard)
+
+        val pointsSum = scratchCards.sumOf { scratchCard ->
+            ScratchCard.getPoints(scratchCard.getWinningNumbers().count())
+        }
+        println("Part 1 - Sample sum: " + pointsSum)
+    }
+
+    part1()
+}
