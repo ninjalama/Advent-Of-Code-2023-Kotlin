0d9ef07ea68c87fec7ebf07c9c4605b63816ecab
Øyvind Høisæther <ninjalama@gmail.com>
2023-12-14 20:48:23 +0100

    Refactored Day 01

--- src/main/kotlin/puzzles/day01/Day01.kt
+++ src/main/kotlin/puzzles/day01/Day01.kt
@@ -14,33 +14,32 @@ data class DigitWord(val stringValue: String, val intValue: Int) {
     }
 }
 
-data class CalibrationDocument(val document: String) {
 
-    fun calculateCalibrationValuePart1(): Int {
-        return document.split("\n").map { line ->
-            // Could just filter by it.isDigit and  take first and last, and build a string out of them but...
-            line.find { it.isDigit() }.toString() + line.findLast { it.isDigit() }.toString()
-        }.map { it.toInt() }
-            .sum()
-    }
-
-    fun calculateCalibrationValuePart2(): Int {
-        val digitWordList = DigitWord.getDigitWordList()
-        return document.split("\n").map { line ->
-            val firstDigitStringOccurence = digitWordList.map { digitWord ->
-                val pos = line.indexOf(digitWord.stringValue)
-                digitWord to if (pos != -1) pos else Int.MAX_VALUE
-            }.minBy { it.second }
-
-            val lastDigitOccurence = digitWordList.map { digitWord ->
-                val pos = line.lastIndexOf(digitWord.stringValue)
-                digitWord to pos
-            }.maxBy { it.second }
-
-            (firstDigitStringOccurence.first.intValue.toString() + lastDigitOccurence.first.intValue.toString()).toInt()
-        }.sum()
-    }
+fun calculateCalibrationValuePart1(document: String): Int {
+    return document.split("\n").map { line ->
+        // Could just filter by it.isDigit and  take first and last, and build a string out of them but...
+        line.find { it.isDigit() }.toString() + line.findLast { it.isDigit() }.toString()
+    }.map { it.toInt() }
+        .sum()
 }
+
+fun calculateCalibrationValuePart2(document: String): Int {
+    val digitWordList = DigitWord.getDigitWordList()
+    return document.split("\n").map { line ->
+        val firstDigitStringOccurence = digitWordList.map { digitWord ->
+            val pos = line.indexOf(digitWord.stringValue)
+            digitWord to if (pos != -1) pos else Int.MAX_VALUE
+        }.minBy { it.second }
+
+        val lastDigitOccurence = digitWordList.map { digitWord ->
+            val pos = line.lastIndexOf(digitWord.stringValue)
+            digitWord to pos
+        }.maxBy { it.second }
+
+        (firstDigitStringOccurence.first.intValue.toString() + lastDigitOccurence.first.intValue.toString()).toInt()
+    }.sum()
+}
+
 
 fun main(args: Array<String>) {
 
@@ -48,28 +47,23 @@ fun main(args: Array<String>) {
     fun part1() {
         // Sample puzzle (Part 1) w/assertion it works
         val sampleInput = ResourceUtils.getResourceAsText("/day01/samplePart1Input.txt").orEmpty()
-        val sampleCalibrationDocument = CalibrationDocument(sampleInput)
         val sampleExpectedSum = 142
-        assert(sampleCalibrationDocument.calculateCalibrationValuePart1() == sampleExpectedSum)
+        assert(calculateCalibrationValuePart1(sampleInput) == sampleExpectedSum)
 
         // Actual Part 1 puzzle
-        val calibrationDocument =
-            CalibrationDocument(ResourceUtils.getResourceAsText("/day01/input.txt").orEmpty())
-        println("Calibration value for Part 1: ${calibrationDocument.calculateCalibrationValuePart1()}")
+        val input = ResourceUtils.getResourceAsText("/day01/input.txt").orEmpty()
+        println("Calibration value for Part 1: ${calculateCalibrationValuePart1(input)}")
     }
 
     fun part2() {
         // Sample puzzle (Part 2) w/assertion it works
         val sampleInput = ResourceUtils.getResourceAsText("/day01/samplePart2Input.txt").orEmpty()
-        val sampleCalibrationDocument = CalibrationDocument(sampleInput)
         val sampleExpectedSum = 142
-        assert(sampleCalibrationDocument.calculateCalibrationValuePart2() == sampleExpectedSum)
+        assert(calculateCalibrationValuePart2(sampleInput) == sampleExpectedSum)
 
-        val calibrationDocument =
-            CalibrationDocument(ResourceUtils.getResourceAsText("/day01/input.txt").orEmpty())
-        println("Calibration value for Part 2: ${calibrationDocument.calculateCalibrationValuePart2()}")
+        val input = ResourceUtils.getResourceAsText("/day01/input.txt").orEmpty()
+        println("Calibration value for Part 2: ${calculateCalibrationValuePart2(input)}")
     }
-
 
     part1()
     part2()
